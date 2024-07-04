7552f08833fe7725614f7fabbcdaccadf3f8340d
Byung Chun Kim <wizardbc@gmail.com>
2024-07-04 11:36:00 +0900

    New layout

--- chatbot.py
+++ chatbot.py
@@ -65,6 +65,7 @@ Args:
   return df[df.similarity >= s].sort_values("similarity", ascending=False).head(top_n)[['section', 'subsection', 'subsubsection', 'text', 'similarity']].to_json()
 
 # stream wrapper
+# gemini does not provide the automatic_function_calling and stream output simultaneously.
 # def gemini_stream_text(response):
 #   for chunk in response:
 #     if parts:=chunk.parts:
@@ -76,39 +77,9 @@ def kwargs2mkdn(_indent:int=0, **kwargs):
   chunk = [' '*_indent + f"- `{k}`: {v}" for k, v in kwargs.items()]
   return '\n'.join(chunk)
 
-col1, col2 = st.columns(2, vertical_alignment='bottom')
-
-with col1:
-  st.title("💬 Chat with Paper")
-  st.caption(":books: \"A Prompt Pattern Catalog to Enhance Prompt Engineering\" with Gemini 1.5")
-  st.write("https://arxiv.org/abs/2302.11382")
-  with st.container(border=True):
-    st.write("논문의 내용을 가져와 대답해 줍니다.")
-    st.write("내용을 가져오기위해 두가지 함수를 사용합니다.\n- `search_from_section_names`: 질문에 대한 대답이 있을 것으로 예상되는 논문의 섹션이름을 추출하여 해당 섹션의 내용을 가져 옵니다.\n- `search_from_text`: 논문의 섹션내용을 나타내는 embedding과 질문의 embedding의 cosine similarity를 사용하여 내용을 가져 옵니다.\n\n언어모델이 어떤 함수를 사용할지 판단하며, 명시적으로 함수명을 언급하여 해당 함수를 사용하도록 지시 할 수도 있습니다.\n\n아래의 목차를 참고하여 문서와 대화 해 보세요.")
-  with st.container(border=True):
-    st.markdown("**Table of Contents:**")
-    with st.expander("Introduction"):
-      st.write("본 논문은 대화형 대규모 언어 모델(LLM)의 응용 분야를 확장하기 위해 프롬프트 패턴을 도입합니다.")
-    with st.expander("Comparing Software Patterns with Prompt Patterns"):
-      st.write("본 섹션에서는 소프트웨어 패턴과 프롬프트 패턴을 비교하여 프롬프트 엔지니어링을 위한 프레임워크를 제시합니다.\n\nSub-sections:\n- Overview of Software Patterns\n- Overview of Prompt Patterns\n- Evaluating Means for Defining a Prompt Pattern's Structure and Ideas\n- A Way Forward: Fundamental Contextual Statements")
-    with st.expander("A Catalog of Prompt Patterns for Conversational LLMs"):
-      st.write("본 섹션에서는 대화형 LLM 상호작용 및 출력 생성을 위한 소프트웨어 작업 자동화를 위한 프롬프트 패턴 카탈로그를 제시합니다.\n\nSub-sections:\n- Summary of the Prompt Pattern Catalog\n- The Meta Language Creation Pattern\n- The Output Automater Pattern\n- The Flipped Interaction Pattern\n- The Persona Pattern\n- The Question Refinement Pattern\n- The Alternative Approaches Pattern\n- The Cognitive Verifier Pattern\n- The Fact Check List Pattern\n- The Template Pattern\n- The Infinite Generation Pattern\n- The Visualization Generator Pattern\n- The Game Play Pattern\n- The Reflection Pattern\n- The Refusal Breaker Pattern\n- The Context Manager Pattern\n- The Recipe Pattern\n\nEach subsection, except the first, has subsubsections:\n- Intent and Context\n- Motivation\n- Structure and Key Ideas\n- Example Implementation\n- Consequences")
-    with st.expander("Related Work"):
-      st.write("본 섹션에서는 소프트웨어 패턴과 프롬프트 설계에 대한 기존 연구 및 LLM, 특히 ChatGPT의 성능 평가를 다룹니다.")
-    with st.expander("Concluding Remarks"):
-      st.write("본 논문은 ChatGPT와 같은 대규모 언어 모델(LLM)을 위한 프롬프트 패턴 카탈로그를 문서화하고 적용하는 프레임워크를 제시하며, 프롬프트 패턴 설계를 개선하여 대화형 LLM을 위한 새로운 기능을 창출하는 연구를 장려하고자 합니다.")
-
 # Memo
 if "memo" not in st.session_state:
   st.session_state.memo = []
-
-with col1:
-  memo = st.container(border=True)
-  memo.subheader("Memo", divider='gray')
-  for m in st.session_state.memo:
-    memo.write(m)
-    memo.button("Remove", on_click=st.session_state.memo.remove, args=[m], key=f'_memo_{hash(m)}')
-    memo.divider()
 
 # Google API key
 if "api_key" not in st.session_state:
@@ -116,8 +87,7 @@ if "api_key" not in st.session_state:
     st.session_state.api_key = st.secrets["GOOGLE_API_KEY"]
   except:
     st.session_state.api_key = ''
-    with col2:
-      st.warning("Your Google API Key is not provided in `.streamlit/secrets.toml`, but you can input one in the sidebar for temporary use.")
+    st.warning("Your Google API Key is not provided in `.streamlit/secrets.toml`, but you can input one in the sidebar for temporary use.")
 
 # Sidebar for parameters
 with st.sidebar:
@@ -128,8 +98,12 @@ with st.sidebar:
     st.session_state.api_key = st.text_input("Google API Key", type="password")
 
   st.subheader("Visible")
-  system_checkbox = st.checkbox("system", value=False)
-  f_call_checkbox = st.checkbox("tool", value=False)
+  help_checkbox = st.checkbox("Help", value=False)
+  toc_checkbox = st.checkbox("Table of Contents", value=True)
+  memo_checkbox = st.checkbox("Memo", value=True)
+  system_checkbox = st.checkbox("System Prompt", value=False)
+  f_call_checkbox = st.checkbox("Function Call", value=False)
+  f_response_checkbox = st.checkbox("Function Response", value=True)
 
   # ChatCompletion parameters
   st.header("Gemini Parameters")
@@ -140,10 +114,57 @@ with st.sidebar:
     "top_k": st.number_input("top_k", min_value=1, value=64),
     "max_output_tokens": st.number_input("max_output_tokens", min_value=1, value=8192),
   }
-  
-  # retriever parameters
-  # st.header("Retriever Parameters")
-  # n_chunks = st.number_input("number of chunks", min_value=1, value=5)
+
+# Layout
+tab_main, tab_memo = st.tabs(["Main", "Memo"])
+
+with tab_main:
+  st.title("💬 Chat with Paper")
+  st.caption(":books: \"A Prompt Pattern Catalog to Enhance Prompt Engineering\" with Gemini 1.5")
+  st.write("https://arxiv.org/abs/2302.11382")
+  st.divider()
+
+  if help_checkbox or toc_checkbox or memo_checkbox:
+    col_l, col_r = st.columns(2, vertical_alignment='bottom')
+    with col_l:
+      messages = st.container()
+  else:
+    messages = st.container()
+
+with tab_memo:
+  st.header("Memo", divider=True)
+  for m in st.session_state.memo:
+    st.write(m)
+    st.divider()
+
+# Help and Memo in col_r
+if help_checkbox:
+  with col_r:
+    with st.container(border=True):
+      st.write("논문의 내용을 가져와 대답해 줍니다.")
+      st.write("내용을 가져오기위해 두가지 함수를 사용합니다.\n- `search_from_section_names`: 질문에 대한 대답이 있을 것으로 예상되는 논문의 섹션이름을 추출하여 해당 섹션의 내용을 가져 옵니다.\n- `search_from_text`: 논문의 섹션내용을 나타내는 embedding과 질문의 embedding의 cosine similarity를 사용하여 내용을 가져 옵니다.\n\n언어모델이 어떤 함수를 사용할지 판단하며, 명시적으로 함수명을 언급하여 해당 함수를 사용하도록 지시 할 수도 있습니다.\n\n아래의 목차를 참고하여 문서와 대화 해 보세요.")
+if toc_checkbox:
+  with col_r:
+    with st.container(border=True):
+      st.markdown("**Table of Contents:**")
+      with st.expander("Introduction"):
+        st.write("본 논문은 대화형 대규모 언어 모델(LLM)의 응용 분야를 확장하기 위해 프롬프트 패턴을 도입합니다.")
+      with st.expander("Comparing Software Patterns with Prompt Patterns"):
+        st.write("본 섹션에서는 소프트웨어 패턴과 프롬프트 패턴을 비교하여 프롬프트 엔지니어링을 위한 프레임워크를 제시합니다.\n\nSub-sections:\n- Overview of Software Patterns\n- Overview of Prompt Patterns\n- Evaluating Means for Defining a Prompt Pattern's Structure and Ideas\n- A Way Forward: Fundamental Contextual Statements")
+      with st.expander("A Catalog of Prompt Patterns for Conversational LLMs"):
+        st.write("본 섹션에서는 대화형 LLM 상호작용 및 출력 생성을 위한 소프트웨어 작업 자동화를 위한 프롬프트 패턴 카탈로그를 제시합니다.\n\nSub-sections:\n- Summary of the Prompt Pattern Catalog\n- The Meta Language Creation Pattern\n- The Output Automater Pattern\n- The Flipped Interaction Pattern\n- The Persona Pattern\n- The Question Refinement Pattern\n- The Alternative Approaches Pattern\n- The Cognitive Verifier Pattern\n- The Fact Check List Pattern\n- The Template Pattern\n- The Infinite Generation Pattern\n- The Visualization Generator Pattern\n- The Game Play Pattern\n- The Reflection Pattern\n- The Refusal Breaker Pattern\n- The Context Manager Pattern\n- The Recipe Pattern\n\nEach subsection, except the first, has subsubsections:\n- Intent and Context\n- Motivation\n- Structure and Key Ideas\n- Example Implementation\n- Consequences")
+      with st.expander("Related Work"):
+        st.write("본 섹션에서는 소프트웨어 패턴과 프롬프트 설계에 대한 기존 연구 및 LLM, 특히 ChatGPT의 성능 평가를 다룹니다.")
+      with st.expander("Concluding Remarks"):
+        st.write("본 논문은 ChatGPT와 같은 대규모 언어 모델(LLM)을 위한 프롬프트 패턴 카탈로그를 문서화하고 적용하는 프레임워크를 제시하며, 프롬프트 패턴 설계를 개선하여 대화형 LLM을 위한 새로운 기능을 창출하는 연구를 장려하고자 합니다.")
+if memo_checkbox:
+  with col_r:
+    memo = st.container(border=True)
+  memo.subheader("Memo", divider=True)
+  for m in st.session_state.memo:
+    memo.write(m)
+    memo.button("Remove", on_click=st.session_state.memo.remove, args=[m], key=f'_memo_{hash(m)}')
+    memo.divider()
 
 safety_settings={
   'harassment':'block_none',
@@ -184,59 +205,53 @@ else:
   )
   chat_session = model.start_chat(enable_automatic_function_calling=True)
   st.session_state.chat_session = chat_session
-  # with st.spinner("Generating..."):
-  #   response = chat_session.send_message("Give me a simplified ToC containing all the section and subsection names, not subsubsection.")
-  # st.rerun()
 
 with st.sidebar:
   # chat controls
   st.header("Chat Control")
-  # chat_role = st.selectbox("role", ["system", "model", "user", "tool"], index=1)
   btn_col1, btn_col2 = st.columns(2)
   with btn_col1:
     st.button("Rewind", on_click=chat_session.rewind, use_container_width=True, type='primary')
   with btn_col2:
     st.button("Clear", on_click=chat_session._history.clear, use_container_width=True)
 
-with col2:
-  messages = st.container()
-  # display system instruction
-  if system_checkbox:
-    with messages.chat_message("system"):
-      st.write(system_instruction)
+# display system instruction
+if system_checkbox:
+  with messages.chat_message("system"):
+    st.write(system_instruction)
 
-  # display messages in history
-  for content in st.session_state.chat_session.history:
-    for part in content.parts:
-      if text:=part.text:
-        with messages.chat_message('human' if content.role == 'user' else 'ai'):
-          st.write(text)
-          if content.role == 'model':
-            st.button("Memo", on_click=st.session_state.memo.append, args=[text], key=f'btn_{hash(text)}')
-      if f_call_checkbox:
-        if fc:=part.function_call:
-          with messages.chat_message('ai'):
-            st.write(f"Function Call\n- name: {fc.name}\n- args\n{kwargs2mkdn(4, **fc.args)}")
-        if fr:=part.function_response:
-          with messages.chat_message('human'):
-            st.write(f"Function Response\n- name: {fr.name}\n- response\n  - `result`")
-            st.json(fr.response["result"])
-      else:
-        if fr:=part.function_response:
-          if fr.name == "get_memo":
-            pass
-          else:
-            with messages.chat_message('retriever', avatar="📜"):
-              retriever_df = pd.read_json(StringIO(fr.response["result"]))
-              st.dataframe(retriever_df.loc[:, (retriever_df.columns != "text")])
+# display messages in history
+for content in st.session_state.chat_session.history:
+  for part in content.parts:
+    if text:=part.text:
+      with messages.chat_message('human' if content.role == 'user' else 'ai'):
+        st.write(text)
+        if content.role == 'model':
+          if memo_checkbox:
+            st.button("Memo", on_click=st.session_state.memo.append, args=[text], key=f'_btn_{hash(text)}')
+    if f_call_checkbox and (fc:=part.function_call):
+      with messages.chat_message('ai'):
+        st.write(f"Function Call\n- name: {fc.name}\n- args\n{kwargs2mkdn(4, **fc.args)}")
+    if f_response_checkbox and (fr:=part.function_response):
+      with messages.chat_message('retriever', avatar="📜"):
+        if "search_" in fr.name:
+          retriever_df = pd.read_json(StringIO(fr.response["result"]))
+          st.dataframe(retriever_df.loc[:, (retriever_df.columns != "text")])
+          with st.expander("Content"):
+            for text in retriever_df.text:
+              st.write(text)
+        else:
+          st.write(f"Function Response\n- name: {fr.name}\n- response\n  - `result`")
+          st.json(fr.response["result"])
 
-  # chat input
-  if prompt := st.chat_input("Ask me anything...", disabled=False if st.session_state.api_key else True):
-    with messages.chat_message('human'):
-      st.write(prompt)
-    with messages.chat_message('ai'):
-      with st.spinner("Generating..."):
-        # response = chat_session.send_message(prompt, stream=True)
-        response = chat_session.send_message(prompt)
-      # st.write_stream(gemini_stream_text(response))
-    st.rerun()
+
+# chat input
+if prompt := st.chat_input("Ask me anything...", disabled=False if st.session_state.api_key else True):
+  with messages.chat_message('human'):
+    st.write(prompt)
+  with messages.chat_message('ai'):
+    with st.spinner("Generating..."):
+      # response = chat_session.send_message(prompt, stream=True)
+      response = chat_session.send_message(prompt)
+    # st.write_stream(gemini_stream_text(response))
+  st.rerun()
